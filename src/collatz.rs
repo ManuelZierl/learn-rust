d915aa14eeeecdb6d27f9df428d9fe3fb03901ee
ru76sin <manuel.zierl@campus.lmu.de>
2019-10-24 12:23:14 +0200

    task 1 collatz conjecture

--- src/collatz.rs
+++ src/collatz.rs
@@ -1,1 +1,29 @@
 // TASK 1: Collatz Conjecture
+
+use std::cmp;
+
+fn f(n:u32) -> u32
+{
+    if n%2 == 0 { return n/2 }
+    return 3*n +1
+}
+
+pub fn collatz_it(mut n:u32) -> (u32,u32)
+{
+    let mut count = 0;
+    let mut max = n;
+    while n != 1
+    {
+        let temp = f(n);
+        max = cmp::max(max, temp);
+        n = temp;
+        count += 1;
+    }
+    return (count, max)
+}
+
+pub fn collatz_re(n:u32, counter:u32) -> u32
+{
+    if n != 1 {collatz_re(f(n), counter+1)}
+    else {counter}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
 mod collatz;
 
-fn main() {
+fn main()
+{
+    test_collatz()
+}
+
+fn test_collatz()
+{
+    for x in 1..=100
+    {
+        let c1 = collatz::collatz_it(x);
+        println!("for n = {}, smallest i: {}, largest n: {}", x, c1.0, c1.1);
+        let c2 = collatz::collatz_re(x, 0);
+        println!("for n = {}, smallest i: {}", x, c2);
+        assert!(c1.0 == c2);
+    }
 }
